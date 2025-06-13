a874259bba38e754c8a370aa52de3f2c4027103b
Robert <robert.goepfert1999@gmail.com>
2025-06-13 18:11:40 +0200

    feat(JSONMapper): unified method signatures

--- src/JSONMapper/JSONMapper.pas
+++ src/JSONMapper/JSONMapper.pas
@@ -8,6 +8,7 @@ uses
   System.TypInfo,
   System.Variants,
   System.SysUtils,
+  System.Generics.Collections,
   JSONMapper.Exceptions,
   JSONMapper.Attributes,
   JSONMapper.ClassFieldHelper,
@@ -15,6 +16,16 @@ uses
   PublicFieldIterator;
 
 type
+  /// <summary> JSONMapper for mapping a generic object to a TJSONObject and vice versa.
+  ///  <para> <c>TJSONString</c> - String, Char, WChar, LString, WString, UString </para>
+  ///  <para> <c>TJSONNumber</c> - Integer, Int64, Float </para>
+  ///  <para> <c>TJSONBool</c> - Boolean </para>
+  ///  <para> <c>TJSONObject</c> - Object, Record </para>
+  ///  <para> <c>TJSONArray</c> - Array, Dynamic Array, TList, TEnumerable in general </para>
+  /// </summary>
+  /// <remarks>
+  /// A Field can be ignored by adding the <c>IgnoreFieldAttribute</c> to it.
+  /// </remarks>
   TJSONMapper = class
   protected
     class function createJSONValue(obj: TObject; rttiField: TRttiField): TJSONValue; overload; static;
@@ -24,22 +35,21 @@ type
     class function recordToJSON(const rec: TValue): TJSONObject; static;
     class function arrayToJSON(const arr: TValue): TJSONArray; static;
   public
-    /// <summary> Maps the public fields of a generic object into a TJSONObject.
-    ///  <para> <c>TJSONString</c> - String, Char, WChar, LString, WString, UString </para>
-    ///  <para> <c>TJSONNumber</c> - Integer, Int64, Float </para>
-    ///  <para> <c>TJSONBool</c> - Boolean </para>
-    ///  <para> <c>TJSONObject</c> - Object, Record </para>
-    ///  <para> <c>TJSONArray</c> - Array, Dynamic Array, TList, TEnumerable in general </para>
-    /// </summary>
-    /// <remarks>
-    /// A Field can be ignored by adding the <c>IgnoreFieldAttribute</c> to it.
-    /// </remarks>
-    class function objectToJSON(const obj: TObject): TJSONObject;
+    /// <summary> Maps the public fields of a generic object into a TJSONObject. </summary>
+    class procedure objectToJSON(const obj: TObject; var jsonObject: TJSONObject); overload;
+    class function objectToJSON(const obj: TObject): TJSONObject; overload;
 
     /// <summary> Maps a generic TList/TEnumerable into a TJSONArray. </summary>
-    class function listToJSON(const list: TObject): TJSONArray;
+    class procedure listToJSON(const list: TObject; var jsonArray: TJSONArray); overload;
+    class function listToJSON(const list: TObject): TJSONArray; overload;
 
-    class function jsonToObject<T: class, constructor>(const jsonObject: TJSONObject): T; static;
+    /// <summary> Maps a TJSONObject into a generic object. </summary>
+    class procedure jsonToObject<T: class>(const jsonObject: TJSONObject; obj: T); overload;
+    class function jsonToObject<T: class, constructor>(const jsonObject: TJSONObject): T; overload;
+
+    /// <summary> Maps a TJSONArray into a generic TList. </summary>
+    class procedure jsonToList<T: class>(const jsonArray: TJSONArray; list: TList<T>); overload;
+    class function jsonToList<T: class, constructor>(const jsonArray: TJSONArray): TList<T>; overload;
   end;
 
   IgnoreFieldAttribute = JSONMapper.Attributes.IgnoreFieldAttribute;
@@ -49,7 +59,19 @@ implementation
 class function TJSONMapper.objectToJSON(const obj: TObject): TJSONObject;
 var
   jsonObject: TJSONObject;
+begin
+  jsonObject := TJSONObject.Create();
+  try
+    objectToJSON(obj, jsonObject);
+  except
+    jsonObject.Free;
+    raise;
+  end;
+  exit(jsonObject);
+end;
 
+class procedure TJSONMapper.objectToJSON(const obj: TObject; var jsonObject: TJSONObject);
+var
   rttiContext: TRttiContext;
   rttiInstanceType: TRttiInstanceType;
   rttiField: TRttiField;
@@ -58,34 +80,24 @@ var
   jsonValue: TJSONValue;
   jsonPair: TJSONPair;
 begin
-  jsonObject := TJSONObject.Create();
+  rttiContext := TRttiContext.Create();
   try
-    rttiContext := TRttiContext.Create();
-    try
-      rttiInstanceType := rttiContext.GetType(obj.ClassType) as TRttiInstanceType;
+    rttiInstanceType := rttiContext.GetType(obj.ClassType) as TRttiInstanceType;
 
-      for rttiField in rttiInstanceType.GetPublicFields() do begin
-        jsonKey := rttiField.Name;
-        jsonValue := createJSONValue(obj, rttiField);
+    for rttiField in rttiInstanceType.GetPublicFields() do begin
+      jsonKey := rttiField.Name;
+      jsonValue := createJSONValue(obj, rttiField);
 
-        jsonPair := TJSONPair.Create(jsonKey, jsonValue);
-        jsonObject.AddPair(jsonPair);
-      end;
-    finally
-      rttiContext.Free;
+      jsonPair := TJSONPair.Create(jsonKey, jsonValue);
+      jsonObject.AddPair(jsonPair);
     end;
-  except
-    jsonObject.Free;
-    raise;
+  finally
+    rttiContext.Free;
   end;
-
-  exit(jsonObject);
 end;
 
-class function TJSONMapper.listToJSON(const list: TObject): TJSONArray;
+class procedure TJSONMapper.listToJSON(const list: TObject; var jsonArray: TJSONArray);
 var
-  jsonArray: TJSONArray;
-
   rttiContext: TRttiContext;
   enumerator: TValue;
   currentProperty: TRttiProperty;
@@ -94,34 +106,40 @@ var
   current: TValue;
   jsonValue: TJSONValue;
 begin
-  jsonArray := TJSONArray.Create();
+  rttiContext := TRttiContext.Create();
   try
-    rttiContext := TRttiContext.Create();
-    try
-      getEnumeratorMethods(
-        list,
-        enumerator,
-        currentProperty,
-        moveNextMethod
-      );
+    getEnumeratorMethods(
+      list,
+      enumerator,
+      currentProperty,
+      moveNextMethod
+    );
 
-      try
-        while moveNextMethod.Invoke(enumerator, []).AsBoolean do begin
-          current := currentProperty.GetValue(enumerator.AsObject);
-          jsonValue := createJSONValue(current);
-          jsonArray.AddElement(jsonValue);
-        end;
-      finally
-        enumerator.AsObject.Free;
+    try
+      while moveNextMethod.Invoke(enumerator, []).AsBoolean do begin
+        current := currentProperty.GetValue(enumerator.AsObject);
+        jsonValue := createJSONValue(current);
+        jsonArray.AddElement(jsonValue);
       end;
     finally
-      rttiContext.Free;
+      enumerator.AsObject.Free;
     end;
+  finally
+    rttiContext.Free;
+  end;
+end;
+
+class function TJSONMapper.listToJSON(const list: TObject): TJSONArray;
+var
+  jsonArray: TJSONArray;
+begin
+  jsonArray := TJSONArray.Create();
+  try
+    listToJSON(list, jsonArray);
   except
     jsonArray.Free;
     raise;
   end;
-
   exit(jsonArray);
 end;
 
@@ -256,7 +274,6 @@ var
   jsonArray: TJSONArray;
 
   rttiContext: TRttiContext;
-  arrayType: TRttiType;
   arrayLength: integer;
 
   i: Integer;
@@ -267,7 +284,6 @@ begin
   try
     rttiContext := TRttiContext.Create();
     try
-      arrayType := rttiContext.GetType(arr.TypeInfo);
       arrayLength := arr.GetArrayLength();
 
       for i := 0 to arrayLength - 1 do begin
@@ -292,29 +308,43 @@ class function TJSONMapper.jsonToObject<T>(
 ): T;
 var
   obj: T;
-
-  rttiContext: TRttiContext;
-  rttiInstanceType: TRttiInstanceType;
-  rttiField: TRttiField;
 begin
   obj := T.Create();
   try
-    rttiContext := TRttiContext.Create();
-    try
-      rttiInstanceType := rttiContext.GetType(T) as TRttiInstanceType;
-
-      for rttiField in rttiInstanceType.GetFields() do begin
-
-      end;
-    finally
-      rttiContext.Free;
-    end;
+    jsonToObject<T>(jsonObject, obj);
   except
     obj.Free;
     raise;
   end;
-
   exit(obj);
+end;
+
+class procedure TJSONMapper.jsonToObject<T>(const jsonObject: TJSONObject; obj: T);
+var
+  rttiContext: TRttiContext;
+  rttiInstanceType: TRttiInstanceType;
+  rttiField: TRttiField;
+begin
+  rttiContext := TRttiContext.Create();
+  try
+    rttiInstanceType := rttiContext.GetType(T) as TRttiInstanceType;
+
+    for rttiField in rttiInstanceType.GetFields() do begin
+
+    end;
+  finally
+    rttiContext.Free;
+  end;
+end;
+
+class function TJSONMapper.jsonToList<T>(const jsonArray: TJSONArray): TList<T>;
+begin
+
+end;
+
+class procedure TJSONMapper.jsonToList<T>(const jsonArray: TJSONArray; list: TList<T>);
+begin
+
 end;
 
 end.
